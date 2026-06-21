9134ad753c37746c6bbefab835c6d21420ea9f6c
arsenijzajcev242-blip <arsenijzajcev242@gmail.com>
2026-06-21 17:16:37 +0300

    Update foe.py

--- foe.py
+++ foe.py
@@ -1,13 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
 TOKEN = '8753338258:AAHkpPFzt6uJMJ2DKUF-_jAvMJI_746WIZ4'
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message()
+@dp.message(CommandStart())
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    await message.answer('Привет')
 
 async def main():
     await dp.start_polling(bot)
