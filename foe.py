024c843ad394fcf5163eae4d8828ef2581bb9270
arsenijzajcev242-blip <arsenijzajcev242@gmail.com>
2026-06-21 17:28:10 +0300

    Update foe.py

--- foe.py
+++ foe.py
@@ -9,7 +9,7 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def echo(message: types.Message):
-    await message.answer('Привет,как дела?')
+    await message.answer('Привет,как ты?')
 
 async def main():
     await dp.start_polling(bot)
